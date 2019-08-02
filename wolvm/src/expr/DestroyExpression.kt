e31d9563eee486568b3bf9fa6e964aea2b3ac03f
Александр Гунгер <alexandergunger666@gmail.com>
2019-08-02 11:53:24 +0300

    Add DestroyExpression and closeble to wolFunction

---
+++ wolvm/src/expr/DestroyExpression.kt
@@ -0,0 +1,10 @@
+package expr
+
+import utils.VMExpression
+import wolvm.wolValue
+
+class DestroyExpression : VMExpression {
+    override fun Call(vararg args: wolValue) {
+        TODO("not implemented")
+    }
+}
--- wolvm/src/wolvm/wolFunction.kt
+++ wolvm/src/wolvm/wolFunction.kt
@@ -6,4 +6,5 @@ class wolFunction {
     var security: SecurityModifer = SecurityModifer.PRIATE
     var return_type: wolClass = wolVoid()
     var body: String = "return <null:void>;"
+    var close: Boolean = false
 }
