241180418e7f8c980048e1111eebeaac15549035
Александр Гунгер <alexandergunger666@gmail.com>
2019-08-04 20:08:11 +0300

    Add Run function for running code

--- wolvm/src/wolvm/VirtualMachine.kt
+++ wolvm/src/wolvm/VirtualMachine.kt
@@ -24,9 +24,17 @@ fun main(args: Array<String>)
             "-info" -> {
                 print("World of Legends Virtual Machine v$version\nAuthor: snaulX\nCopyright (c) 2019")
             }
-            "-run" -> {
-                //pass
-            }
+            /*"-encode" -> {
+                lateinit var bytes: ByteArray
+                try
+                {
+                    bytes = File(args[1]).readBytes()
+                }
+                catch (FileNotFoundException)
+                {
+
+                }
+            }*/
             "-help" -> {
                 println("World of Legends Virtual Machine Helper")
                 println()
@@ -35,118 +43,119 @@ fun main(args: Array<String>)
                 println("<full file name> ; run build-file")
             }
             else -> {
-                //create context of thread
-                mainstack.classes.put("void", wolVoid())
-                mainstack.classes.put("bool", wolBool())
-                mainstack.classes.put("byte", wolByte())
-                mainstack.classes.put("short", wolShort())
-                mainstack.classes.put("int", wolInt())
-                mainstack.classes.put("float", wolFloat())
-                mainstack.classes.put("long", wolLong())
-                mainstack.classes.put("double", wolDouble())
-
                 lateinit var reader: FileReader
-                try
-                {
+                try {
                     reader = FileReader(args[0])
-                }
-                catch (e: FileNotFoundException)
-                {
+                } catch (e: FileNotFoundException) {
                     throwVMException("File by full name ${args[0]} not found", 0, ExceptionType.FileNotFoundException)
                 }
                 val input: String = reader.readText()
+                Run(input)
+            }
+        }
+    }
+}
 
-                //main cycle
-                var position: Int = 0
-                var current: Char = input[position]
-                while (position < input.length)
+fun Run(code: String)
+{
+    //create context of thread
+    mainstack.classes.put("void", wolVoid())
+    mainstack.classes.put("bool", wolBool())
+    mainstack.classes.put("byte", wolByte())
+    mainstack.classes.put("short", wolShort())
+    mainstack.classes.put("int", wolInt())
+    mainstack.classes.put("float", wolFloat())
+    mainstack.classes.put("long", wolLong())
+    mainstack.classes.put("double", wolDouble())
+
+    //main cycle
+    var position: Int = 0
+    var current: Char = code[position]
+    while (position < code.length)
+    {
+        while (current.isWhitespace()) //skip whitespaces
+        {
+            try {
+                current = code[++position]
+            }
+            catch (e: IndexOutOfBoundsException)
+            {
+                throwVMException("End of parsing not found", position, ExceptionType.BLDSyntaxException)
+                break
+            }
+        }
+        var buffer: StringBuilder = StringBuilder()
+        while (!current.isWhitespace()) //get word
+        {
+            try {
+                buffer.append(current)
+                current = code[++position]
+            }
+            catch (ex: IndexOutOfBoundsException)
+            {
+                throwVMException("End of parsing not found", position, ExceptionType.BLDSyntaxException)
+            }
+        }
+        when (buffer.toString())
+        {
+            "_loads" -> {
+                current = code[++position]
+                if (current == '{')
                 {
-                    while (current.isWhitespace()) //skip whitespaces
-                    {
-                        try {
-                            current = input[++position]
-                        }
-                        catch (ex: IndexOutOfBoundsException)
-                        {
-                            throwVMException("End of parsing not found", position, ExceptionType.BLDSyntaxException)
-                            break
-                        }
-                    }
-                    var buffer: StringBuilder = StringBuilder()
-                    while (!current.isWhitespace()) //get word
-                    {
-                        try {
-                            buffer.append(current)
-                            current = input[++position]
-                        }
-                        catch (ex: IndexOutOfBoundsException)
-                        {
-                            throwVMException("End of parsing not found", position, ExceptionType.BLDSyntaxException)
-                        }
-                    }
-                    when (buffer.toString())
-                    {
-                        "_loads" -> {
-                            current = input[++position]
-                            if (current == '{')
-                            {
-                                //valid
-                            }
-                            else
-                            {
-                                throwVMException("Start of _loads block not found", position, ExceptionType.BLDSyntaxException)
-                            }
-                        }
-                        "main" -> {
-                            current = input[++position]
-                            if (current == '{')
-                            {
-                                while (current != '}') {
-                                    try {
-                                        buffer.append(current)
-                                        current = input[++position]
-                                    } catch (ex: IndexOutOfBoundsException) {
-                                        throwVMException("End of script not found", position, ExceptionType.BLDSyntaxException)
-                                    }
-                                }
-                                parse(buffer.toString())
-                            }
-                            else
-                            {
-                                throwVMException("Start of script not found", position, ExceptionType.BLDSyntaxException)
-                            }
-                        }
-                        "stack" -> {
-                            current = input[++position]
-                            if (current == '{')
-                            {
-                                val start = {
-                                    while (current != '}') {
-                                        try {
-                                            buffer.append(current)
-                                            current = input[++position]
-                                        } catch (ex: IndexOutOfBoundsException) {
-                                            throwVMException("End of stack not found", position, ExceptionType.BLDSyntaxException)
-                                        }
-                                    }
-                                }
-                                if (input[position + 1] == ';')
-                                {
-                                    run(start) //kotlin why you haven`t 'goto' ((
-                                }
-                                Stack.parse(buffer.toString())
-                            }
-                            else
-                            {
-                                throwVMException("Start of stack not found", position, ExceptionType.BLDSyntaxException)
-                            }
-                        }
-                        "end" -> return
-                        else ->
-                            throwVMException("Unknown keyword $buffer", position, ExceptionType.BLDSyntaxException)
-                    }
+                    //valid
+                }
+                else
+                {
+                    throwVMException("Start of _loads block not found", position, ExceptionType.BLDSyntaxException)
                 }
             }
+            "main" -> {
+                current = code[++position]
+                if (current == '{')
+                {
+                    while (current != '}') {
+                        try {
+                            buffer.append(current)
+                            current = code[++position]
+                        } catch (e: IndexOutOfBoundsException) {
+                            throwVMException("End of script not found", position, ExceptionType.BLDSyntaxException)
+                        }
+                    }
+                    parse(buffer.toString())
+                }
+                else
+                {
+                    throwVMException("Start of script not found", position, ExceptionType.BLDSyntaxException)
+                }
+            }
+            "stack" -> {
+                current = code[++position]
+                if (current == '{')
+                {
+                    val start = {
+                        while (current != '}') {
+                            try {
+                                buffer.append(current)
+                                current = code[++position]
+                            } catch (ex: IndexOutOfBoundsException) {
+                                throwVMException("End of stack not found", position, ExceptionType.BLDSyntaxException)
+                            }
+                        }
+                    }
+                    if (code[position + 1] == ';')
+                    {
+                        run(start) //kotlin why you haven`t 'goto' ((
+                    }
+                    Stack.parse(buffer.toString())
+                }
+                else
+                {
+                    throwVMException("Start of stack not found", position, ExceptionType.BLDSyntaxException)
+                }
+            }
+            "end" -> return
+            else ->
+                throwVMException("Unknown keyword $buffer", position, ExceptionType.BLDSyntaxException)
         }
     }
 }
